0ea76578cd87fcc05311301f50199cb64b93045e
Matte <mattias.johansson@atex.com>
2016-04-08 12:56:22 +0200

    Re-added cbtail

--- cbtail.rb
+++ cbtail.rb
@@ -5,15 +5,12 @@
 class Cbtail < Formula
   desc ""
   homepage ""
-  url "https://github.com/folkol/homebrew-tap/blob/master/releases/cbtail-0.1.1.tar.gz"
-  version "0.1.1"
-#  sha256 "7eb1712bc65ede08b1169a46f3892f1209ed48f9c2edd06c8be11afd2a68a561"
+  url "https://github.com/folkol/homebrew-tap/raw/master/releases/cbtail-0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 "7eb1712bc65ede08b1169a46f3892f1209ed48f9c2edd06c8be11afd2a68a561"
 
   # depends_on "cmake" => :build
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
     bin.install cbtail
   end
 end
