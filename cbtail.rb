582019758fa82b5b533915cadb8763d2551c6ae1
Matte <mattias.johansson@atex.com>
2016-04-08 13:57:48 +0200

    Release 0.0.3

--- cbtail.rb
+++ cbtail.rb
@@ -5,9 +5,9 @@
 class Cbtail < Formula
   desc ""
   homepage ""
-  url "https://github.com/folkol/homebrew-tap/raw/master/releases/cbtail-0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "af550161b712c73f8bd1400e01bc8aa7796e13dd52ddf52281cb20c99516b260"
+  url "https://github.com/folkol/homebrew-tap/raw/master/releases/cbtail-0.0.3.tar.gz"
+  version "0.0.3"
+  sha256 "e28bf7d9fcc22cdfde0c7c8f31e3648a4847e7bda9cb69f309f24257eee3dd41"
 
   # depends_on "cmake" => :build
   def install
