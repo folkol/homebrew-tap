7ee06ff909903f015af9c63fe3cb92eec80e2336
Matte <mattias4@kth.se>
2016-04-09 02:12:26 +0200

    Updated cbtail.rb

--- cbtail.rb
+++ cbtail.rb
@@ -1,8 +1,8 @@
 class Cbtail < Formula
   desc ""
   homepage ""
-  url "https://github.com/folkol/cbtail/archive/0.0.8.tar.gz"
-  version "0.0.8"
+  url "https://github.com/folkol/cbtail/archive/0.0.9.tar.gz"
+  version "0.098"
   sha256 "4cb02e7434b28b4f64d05985c1e23dace0f0e8ab2641a656948d0bd26b07c925"
 
   # depends_on "cmake" => :build
