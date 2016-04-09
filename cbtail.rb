dc8eb65eb5a3e0e68d2e67be2c40108cb3ba0288
Matte <mattias4@kth.se>
2016-04-09 02:27:23 +0200

    Updated cbtail.rb

--- cbtail.rb
+++ cbtail.rb
@@ -6,7 +6,7 @@ class Cbtail < Formula
   sha256 "ba2c6260a3656531ffc6b995ac4618cc3b9b5ec9a53a7d29befe54d572b33f1a"
 
   # depends_on "cmake" => :build
-  depends_on "coreutils" => :build
+  depends_on "coreutils"
 
   def install
     prefix.install 'cbtail.jar'
