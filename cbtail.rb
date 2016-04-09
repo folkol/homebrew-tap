a3908afae240a2195e71db674d0e9d50716714c7
Matte <mattias4@kth.se>
2016-04-09 02:28:08 +0200

    Updated cbtail.rb

--- cbtail.rb
+++ cbtail.rb
@@ -2,7 +2,7 @@ class Cbtail < Formula
   desc ""
   homepage ""
   url "https://github.com/folkol/cbtail/archive/0.0.11.tar.gz"
-  version "0.0.11"
+  version "0.0.12"
   sha256 "ba2c6260a3656531ffc6b995ac4618cc3b9b5ec9a53a7d29befe54d572b33f1a"
 
   # depends_on "cmake" => :build
