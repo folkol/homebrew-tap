e3f6f218cb62ccc6f39895b97fad7690ed5ebc19
Matte <mattias.johansson@atex.com>
2016-04-21 09:08:13 +0200

    Added sedsed 1.0

---
+++ sedsed.rb
@@ -0,0 +1,11 @@
+class Sedsed < Formula
+  desc "sed debugger, indenter, tokenizer and color highlighter written by Aurelio Jargas (GPL)"
+  homepage "http://aurelio.net/projects/sedsed/"
+  url "https://github.com/folkol/homebrew-tap/archive/releases/sedsed/1.0.tar.gz"
+  version "1.0"
+  sha256 "535e1fb2cba844a5d098f4f6c121fb9cd23212c2c6ef6cd12badf57224a0709e"
+
+  def install
+    bin.install 'sedsed'
+  end
+end
