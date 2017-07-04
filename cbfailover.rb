3dd1e1d92927db86803cb564ad2c944ed68c10ff
Matte <mattias4@kth.se>
2017-07-04 16:41:32 +0200

    New release of cbfailover

--- cbfailover.rb
+++ cbfailover.rb
@@ -1,15 +1,15 @@
 class Cbfailover < Formula
   desc ""
   homepage ""
-  url "https://github.com/folkol/cb-failover-log-logger/archive/0.4.tar.gz"
-  version "0.0.5"
-  sha256 "f3d8fae382eb6e274ac63a2e396552b81eb59ee18fb6f2751993f665159b4459"
+  url "https://github.com/folkol/cb-failover-log-logger/archive/0.6.tar.gz"
+  version "0.0.6"
+  sha256 "2816d37b88c1fb40e273ebce4c8e5d9c77d85e29b6c7b5f84368ed0ddba9cb0d"
 
   # depends_on "cmake" => :build
   depends_on "coreutils"
 
   def install
-    prefix.install 'target/zk-failover-log-logger-jar-with-dependencies.jar'
+    prefix.install 'target/ck-failover-log-logger-jar-with-dependencies.jar'
     bin.install 'scripts/cbfailover'
   end
 end
