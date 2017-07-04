e351b4281b3c2610c1f5920d19c78914d0659efe
Matte <mattias4@kth.se>
2017-07-04 16:17:38 +0200

    Fixed broken build

--- cbfailover.rb
+++ cbfailover.rb
@@ -1,9 +1,9 @@
 class Cbfailover < Formula
   desc ""
   homepage ""
-  url "https://github.com/folkol/cb-failover-log-logger/archive/0.3.tar.gz"
-  version "0.0.4"
-  sha256 "1693fe08cb4c063005ae199f2918a07c132ae37b6305b74be9dde859ef25d160"
+  url "https://github.com/folkol/cb-failover-log-logger/archive/0.4.tar.gz"
+  version "0.0.5"
+  sha256 "f3d8fae382eb6e274ac63a2e396552b81eb59ee18fb6f2751993f665159b4459"
 
   # depends_on "cmake" => :build
   depends_on "coreutils"
