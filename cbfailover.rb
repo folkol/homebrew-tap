c6126f141ddbe5d885b679b926420a720f236ff4
Matte <mattias4@kth.se>
2017-07-04 16:07:34 +0200

    Added cbfailover

---
+++ cbfailover.rb
@@ -0,0 +1,15 @@
+class Cbtail < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/folkol/cb-failover-log-logger/archive/0.3.tar.gz"
+  version "0.0.3"
+  sha256 "031dd392e8f65da43b8fd0d50091d3801f1b9439fb0a4372d68436141800150c"
+
+  # depends_on "cmake" => :build
+  depends_on "coreutils"
+
+  def install
+    prefix.install 'target/zk-failover-log-logger-jar-with-dependencies.jar'
+    bin.install 'scripts/cbfailover'
+  end
+end
