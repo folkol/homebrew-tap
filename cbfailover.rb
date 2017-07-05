d9229d6fe8c50709674ba3840b04bb33ec8dc266
Matte <mattias4@kth.se>
2017-07-05 15:28:50 +0200

    cbfailover 0.8

--- cbfailover.rb
+++ cbfailover.rb
@@ -1,9 +1,9 @@
 class Cbfailover < Formula
   desc ""
   homepage ""
-  url "https://github.com/folkol/cb-failover-log-logger/archive/0.7.tar.gz"
-  version "0.0.7"
-  sha256 "0f0086a61161de7c81932940529fe8e0b1440cf75ffc655051d14cad551db14d"
+  url "https://github.com/folkol/cb-failover-log-logger/archive/0.8.tar.gz"
+  version "0.0.8"
+  sha256 "016ec8ac667f5e048a9ac31da85bb94660674ef96d643cf6d2b262f47bad5817"
 
   # depends_on "cmake" => :build
   depends_on "coreutils"
